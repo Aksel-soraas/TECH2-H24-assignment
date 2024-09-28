435d0290d0a2694fb23f92b39e6b88733ceef6a3
Aksel Soraas <aksel.soraas@student.nhh.no>
2024-09-28 12:36:29 +0200

    Corrected code in functions

--- part_A.py
+++ part_A.py
@@ -22,27 +22,28 @@ def std_loops(x):
 
     # Define variables to be used in the function
 
-    num_lst = [1, 2, 3, 4, 5]               #Sequence to be deviated THIS SHOULD BE PUT IN MAIN FUNCTION
     sum_lst = 0                             # Sum of list
     num_lst_squared = []                    # Sequence squared to be used in order to calculate the mean of squares
     sum_lst_squared = 0                     # Sum of the list squared
-    N = num_lst[-1]                         # Last number in list
+    N = x[-1]                         # Last number in list
     
-    for i in num_lst:                       # Summarizing sequence
-        sum_lst = sum_lst + num_lst[i-1]
+    for i in x:                       # Summarizing sequence
+        sum_lst = sum_lst + i
 
-    x = (1/N) * sum_lst                     #calculate the mean x
+    m = (1/N) * sum_lst                     #calculate the mean x
 
     #Square all elements in num_list and sum together
 
-    for i in num_lst:
-        num_lst_squared.append(num_lst[i-1]**2)
-        sum_lst_squared = sum_lst_squared + num_lst_squared[i-1]
+    for i in x:
+        num_lst_squared.append(i**2)
+    
+    for i in num_lst_squared:
+        sum_lst_squared = sum_lst_squared + i
 
     # Calculate the mean of suqres S
     S = (1/N) * sum_lst_squared
 
-    O2 = S - x**2                           # Computing the variance O2
+    O2 = S - m**2                           # Computing the variance O2
 
     O = sqrt(O2)                            # computing the standard deviation O 
 
@@ -65,25 +66,36 @@ def std_builtin(x):
     """
 
     # Define variables to be used in the function
-    num_lst = [1, 2, 3, 4, 5]
-    sum_lst = sum(num_lst)
+    sum_lst = sum(x)
     num_lst_squared = []
-    N = len(num_lst)
+    N = len(x)
 
-    x = (1/N) * sum_lst                     #calculate the mean x
+    m = (1/N) * sum_lst                     #calculate the mean m
 
-        #Square all elements in num_list and sum together
+    #Square all elements in num_list and sum together
+    for i in x:
+        num_lst_squared.append(i**2)
 
-    for i in num_lst:
-        num_lst_squared.append(num_lst[i-1]**2)
-        sum_lst_squared = sum(num_lst_squared)
-        # Calculate the mean of suqres S
+    sum_lst_squared = sum(num_lst_squared)
+
+    # Calculate the mean of suqres S
     S = (1/N) * sum_lst_squared
 
-    O2 = S - x**2                           # Computing the variance O2
+    O2 = S - m**2                           # Computing the variance O2
 
     O = sqrt(O2)                            # computing the standard deviation O 
 
-def main():
+    return O
 
-# sum fuction print(sum_sequence.sum())
+def main():
+    x = [1, 2, 3, 4, 5]
+    
+    looped = std_loops(x)
+
+    builtin = std_builtin(x)
+    
+    print(f'Deviation by loops is {looped}, and by built in functions {builtin}')
+    return looped, builtin
+
+if __name__ == "__main__":
+    main()
